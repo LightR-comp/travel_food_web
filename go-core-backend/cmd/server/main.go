a8c2a739454a2572f8adbd150fc87154ae1ec552
Nhut <lecongminhnhut6533@gmail.com>
2026-04-01 20:51:10 +0700

    Khởi tạo Go Server

--- go-core-backend/cmd/server/main.go
+++ go-core-backend/cmd/server/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"go-core-backend/internal/config"
+
+	"github.com/gin-gonic/gin"
+)
+func main() {
+	// Load cấu hình
+	config.LoadConfig()
+
+	// Khởi tạo server Gin mặc định
+	r :=gin.Default()
+
+	// Thêm dòng này để tắt cảnh báo "Trusted Proxies"
+	r.SetTrustedProxies(nil)
+
+	// Tạo endpoint test /ping
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"message": "Go Server is running",
+			"data": nil,
+			"error": nil,
+		})
+	})
+		
+	//Lấy port từ file config
+	port := config.AppConfig.Port
+	fmt.Print("Server is running on port: ", port)
+
+	// Chạy server trên port đã cấu hình
+	if err := r.Run(":" + port); err != nil {
+		fmt.Print("Failed to start server: ", err)
+	}
+}

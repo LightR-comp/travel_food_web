b092df65618536d4d1348f7ffbc76825756ee239
Nhut <lecongminhnhut6533@gmail.com>
2026-04-01 21:35:58 +0700

    định nghĩa các endpoint và gắn router vào main.go

--- go-core-backend/internal/handlers/chatbot.go
+++ go-core-backend/internal/handlers/chatbot.go
@@ -1,1 +1,19 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ChatbotProcess là API xử lý luồng chat với user
+// TODO: Minh viết logic nhận câu hỏi, chọc Python phân tích Intent, gọi DB rồi trả text về ở đây!
+// ChatbotProcess là hàm xử lý cho API /chat
+func ChatbotProcess(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "Chatbot đang phân tích câu hỏi của user...",
+		"data":    nil,
+		"error":   nil,
+	})
+}

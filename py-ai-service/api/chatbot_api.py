9e93590823fece1363a2de106082ed69c92a7708
LightR <quangsangnguyen2006@gmail.com>
2026-04-27 13:18:15 +0700

    [ADD] conftest.py và test_chatbot.py để test API của chatbot, [UPDATE] sửa block I/o trong chatbot_api, promting mới cho nlp_parser.py và thêm cache để lưu tìm kiếm cũ, exception cho consultant_rag và phân loại mức độ safe trước khi response

--- py-ai-service/api/chatbot_api.py
+++ py-ai-service/api/chatbot_api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, APIRouter
 from schemas.payloads import *
 from ai_chatbot.nlp_parser import detect_intent_with_ai
 from ai_chatbot.consultant_rag import generate_final_response
+import asyncio
 
 app = FastAPI()
 router = APIRouter()
@@ -10,7 +11,7 @@ router = APIRouter()
 async def intent_parse_endpoint(request: ChatIntentRequest):
     try:
         # Hàm detect_intent_with_ai tự lấy shared_model từ core
-        result = detect_intent_with_ai(request.message)
+        result = await asyncio.to_thread(detect_intent_with_ai, request.message)
         data = IntentData(
             intent=result.get("intent", "none"),
             entities=result.get("entities", {}),
@@ -24,12 +25,13 @@ async def intent_parse_endpoint(request: ChatIntentRequest):
 async def generate_response_endpoint(request: ChatGenerationRequest):
     try:
         # Hàm generate_final_response tự lấy shared_model từ core
-        final_data = generate_final_response(request)
+        final_data = await asyncio.to_thread(generate_final_response, request)
         return BaseResponse(success=True, message="Tạo phản hồi thành công", data=final_data)
     except Exception as e:
         return BaseResponse(success=False, error=str(e))
 
 app.include_router(router)
+
 
 if __name__ == "__main__":
     import uvicorn

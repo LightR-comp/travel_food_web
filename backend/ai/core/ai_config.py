44d4ac95f85fdfe0cf55a1fdef887e97ac5ed471
LightR <quangsangnguyen2006@gmail.com>
2026-05-10 13:52:20 +0700

    [FIX] Thêm trong .gitignore, đổi lại URL trong axiosInstance và vite.config để đúng với cổng backend

--- backend/ai/core/ai_config.py
+++ backend/ai/core/ai_config.py
@@ -4,4 +4,4 @@ import os
 
 genai.configure(api_key=os.getenv("GEMINI_KEY"))
 # Khởi tạo 1 lần dùng cho cả hội
-shared_model = genai.GenerativeModel('gemini-1.5-flash')
+shared_model = genai.GenerativeModel('gemini-1.5-pro')

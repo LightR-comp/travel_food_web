96e09c4511397c4855ee1f918781d34c29dafb52
minhvonhat <minhvonhat234@gmail.com>
2026-05-20 19:27:07 +0700

    Chỉnh logic bảng DishImages và thêm 1 file khởi tạo tài khoản khách

--- backend/database/DishImages.sql
+++
@@ -1,9 +0,0 @@
-use travel_food_db
-go
-CREATE TABLE DishImages (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    menu_item_id INT NOT NULL FOREIGN KEY REFERENCES MenuItems(id) ON DELETE CASCADE,
-    image_url NVARCHAR(500) NOT NULL,
-    created_at DATETIME DEFAULT GETDATE()
-)
-GO

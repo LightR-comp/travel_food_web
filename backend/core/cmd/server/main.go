71631868fbe3ea146c323c5037cc34eff7c50ab4
lecongminhnhut <lecongminhnhut6533@gmail.com>
2026-05-27 01:02:08 +0700

    Update main.go

--- backend/core/cmd/server/main.go
+++ backend/core/cmd/server/main.go
@@ -43,7 +43,7 @@ func main() {
 
 	// Cấu hình CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:3000","https://travelfoodweb1.vercel.app/"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:3000","https://travelfoodweb1.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},

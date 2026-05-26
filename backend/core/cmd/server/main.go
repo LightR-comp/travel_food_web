e371692901491403c448a5831df8a6bb9af5a720
lecongminhnhut <lecongminhnhut6533@gmail.com>
2026-05-27 01:03:30 +0700

    Add 'X-Requested-With' to CORS allowed headers

--- backend/core/cmd/server/main.go
+++ backend/core/cmd/server/main.go
@@ -45,7 +45,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:3000","https://travelfoodweb1.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	}))
